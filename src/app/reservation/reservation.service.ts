f9a486cd65f7bf336c7b80f3c1415d025b9908c4
QuyNguyen <quynv.mr@gmail.com>
2023-10-28 00:27:20 +0700

    update

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import {Reservation} from "../models/reservation";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private  reservations: Reservation[] = [];
+
+  constructor() {
+    let savedReservations = localStorage.getItem("reservations");
+    this.reservations = savedReservations? JSON.parse(savedReservations) : [];
+  }
+
+  getReservations(): Reservation[] {
+    return this.reservations
+  }
+
+  getReservation(id: string): Reservation | undefined{
+    return this.reservations.find(res => res.id === id);
+  }
+
+  addReservation(reservation:Reservation){
+    this.reservations.push(reservation);
+    localStorage.setItem("reservations",JSON.stringify(this.reservations));
+  }
+
+  deleteReservation(id:string){
+    let index = this.reservations.findIndex(s => s.id === id);
+    this.reservations.splice(index, 1);
+    localStorage.setItem("reservations",JSON.stringify(this.reservations));
+  }
+
+  updateReservation(updatedReservation:Reservation){
+    let index = this.reservations.findIndex(s => s.id === updatedReservation.id);
+    this.reservations[index] = updatedReservation;
+    localStorage.setItem("reservations",JSON.stringify(this.reservations));
+  }
 }
